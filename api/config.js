b9b5f74bb8529bf723bb3a0b0ee1cc4b237a9422
aapa96 <aapa96@gmail.com>
2018-11-01 23:44:45 -0500

    rrr

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3977,
+    port: process.env.PORT || 3000,
     db: process.env.MONGODB_URI || 'mongodb://localhost:27017/open_peru',
     SECRET_TOKEN: 'miclavedetokens'
   }
